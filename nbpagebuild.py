4f3d0a55010d33fc93652672625d84e461f8b3f4
philippjfr <P.Rudiger@ed.ac.uk>
2014-05-15 17:48:39 +0100

    Added nbpagebuild.py to create rst pages for static notebooks

---
+++ nbpagebuild.py
@@ -0,0 +1,21 @@
+"""
+Auto-generates the rst files corresponding to the Notebooks in Tutorials.
+"""
+import os
+from test_notebooks import NotebookFinder
+
+doc_path = os.path.abspath(os.path.join(__file__, '..', '..'))
+file_list = NotebookFinder(os.path.join(doc_path, 'notebook.json'),
+                           projects=[], suites=[]).files
+
+
+for (project, _), files in file_list:
+    for f in files:
+        if not f.startswith(doc_path):
+            continue
+        basename = os.path.basename(f)
+        rst_path = f[:-len('ipynb')].replace(' ', '_') + 'rst'
+        with open(rst_path, 'w') as rst_file:
+            rst_file.write(".. notebook:: %s %s" % (project, basename))
+
+
