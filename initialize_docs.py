02227bbe127088a4ad800fc3520fc99531faf93c
philippjfr <P.Rudiger@ed.ac.uk>
2014-05-16 15:40:36 +0100

    Moved template back out of docbuilder (cannot be shared)

--- initialize_docs.py
+++ initialize_docs.py
@@ -2,7 +2,6 @@ import os, sys, shutil, glob, re
 
 STATIC_DIR = '_static'
 MAKEFILE = 'Makefile'
-TEMPLATE = 'template_page.html'
 
 def get_project_doc(project):
     # Write project specific Makefile
@@ -16,17 +15,6 @@ def get_project_doc(project):
     makefile_dest = os.path.abspath(os.path.join(dest_path, MAKEFILE))
     with open(makefile_dest, 'w') as mf:
         mf.write(make)
-
-    # Write project specific template
-    template_path = os.path.abspath(os.path.join(src_path, TEMPLATE))
-    with open(template_path, 'r') as templatefile:
-        template = templatefile.read()
-    template = template.replace('IOAMPROJECT_lower', project.lower())
-    template = template.replace('IOAMPROJECT', project)
-    template_dest = os.path.join(dest_path, '_templates')
-    if not os.path.isdir(template_dest): os.mkdir(template_dest)
-    with open(os.path.join(template_dest, 'page.html'), 'w') as tf:
-        tf.write(template)
 
     static_dest = os.path.join(dest_path, '_static')
     if not os.path.isdir(static_dest): os.mkdir(static_dest)
