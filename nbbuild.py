3f8e3196f913f6d3cc9dfd3199d00b2f4c13d94f
philippjfr <P.Rudiger@ed.ac.uk>
2015-02-20 18:22:32 +0000

    Fixed IPython profile path

--- nbbuild.py
+++ nbbuild.py
@@ -162,7 +162,8 @@ def evaluate_notebook(nb_path, dest_path=None, skip_exceptions=False):
     cwd = os.getcwd()
     filedir, filename = os.path.split(nb_path)
     os.chdir(filedir)
-    nb_runner = NotebookRunner(notebook, pylab=False, profile_dir='./nbpublisher/profile_docs')
+    profile_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'profile_docs')
+    nb_runner = NotebookRunner(notebook, pylab=False, profile_dir=profile_dir)
     try:
         nb_runner.run_notebook(skip_exceptions=skip_exceptions)
     except NotebookError as e:
