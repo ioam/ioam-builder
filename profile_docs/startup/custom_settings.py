388d5d5bcf84fbbef463fb985891760b3d6b2b63
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-02-10 03:17:56 +0000

    Exporting data to json files

--- profile_docs/startup/custom_settings.py
+++ profile_docs/startup/custom_settings.py
@@ -6,4 +6,9 @@ import numpy     # pyflakes:ignore (API import)
 ip = get_ipython()  # pyflakes:ignore (IPython namespace)
 ip.extension_manager.load_extension('holoviews.ipython')
 
+from holoviews.plotting.widgets import NdWidget
+NdWidget.export_json=True
+NdWidget.json_load_path = ''
+NdWidget.json_save_path = './'
+
 holoviews.plotting.MPLPlot.fig_alpha = 0
