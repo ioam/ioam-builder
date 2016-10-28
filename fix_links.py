f8685fee7691375a064d4f4265fd826cdbe3d3cd
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-10-28 16:59:26 +0100

    fix_links.py also fixes BokehJS

--- fix_links.py
+++ fix_links.py
@@ -7,9 +7,18 @@ Cleans up relative cross-notebook links by replacing them with
 import os
 from bs4 import BeautifulSoup
 
+BOKEH_REPLACEMENTS = {'">\n\n(function(global) {\n  function now()':
+                      '">\n<script>\n(function(global) {\n  function now()',
+                      'run_inline_js();\n    });\n  }\n}(this));\n</div>':
+                      'run_inline_js();\n    });\n  }\n}(this));\n</script>\n</div>'}
+
 def cleanup_links(path):
     with open(path) as f:
-        soup = BeautifulSoup(f.read())
+        text = f.read()
+    if 'BokehJS successfully loaded.' in text:
+        for k, v in BOKEH_REPLACEMENTS.items():
+            text = text.replace(k, v)
+    soup = BeautifulSoup(text)
     for a in soup.findAll('a'):
         href = a.get('href', '')
         if '.ipynb' in href and 'http' not in href:
