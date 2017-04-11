260db18c5bc4cecb67c6b6ca3bef6c3a039d7a14
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-04-11 21:44:28 +0100

    Fixed HTML cleanup script for bokeh notebooks

--- fix_links.py
+++ fix_links.py
@@ -7,15 +7,15 @@ Cleans up relative cross-notebook links by replacing them with
 import os
 from bs4 import BeautifulSoup
 
-BOKEH_REPLACEMENTS = {'">\n\n(function(global) {\n  function now()':
-                      '">\n<script>\n(function(global) {\n  function now()',
-                      'run_inline_js();\n    });\n  }\n}(this));\n</div>':
-                      'run_inline_js();\n    });\n  }\n}(this));\n</script>\n</div>'}
+BOKEH_REPLACEMENTS = {'cell.output_area.append_execute_result':
+                      '//cell.output_area.append_execute_result',
+                      '}(this));\n</div>': '}(this));\n</script></div>',
+                      '\n(function(global) {': '<script>\n(function(global) {'}
 
 def cleanup_links(path):
     with open(path) as f:
         text = f.read()
-    if 'BokehJS successfully loaded.' in text:
+    if 'BokehJS does not appear to have successfully loaded' in text:
         for k, v in BOKEH_REPLACEMENTS.items():
             text = text.replace(k, v)
     soup = BeautifulSoup(text)
@@ -24,7 +24,7 @@ def cleanup_links(path):
         if '.ipynb' in href and 'http' not in href:
             a['href'] = href.replace('.ipynb', '.html')
     html = soup.prettify("utf-8")
-    with open(path, 'wb') as f:
+    with open(path, 'w') as f:
         f.write(html)
 
 
@@ -38,4 +38,7 @@ if __name__ == '__main__':
     for root, dirs, files in os.walk(args.build_dir):
         for file_path in files:
             if file_path.endswith(".html"):
-                soup = cleanup_links(os.path.join(root, file_path))
+                try:
+                    soup = cleanup_links(os.path.join(root, file_path))
+                except:
+                    pass
