8becce8dc71c776462f168dfa82e8962d9515b79
C. E. Ball <ceball@gmail.com>
2017-11-13 16:47:33 +0000

    fix copy paste error

--- ioam_theme/setup.py
+++ ioam_theme/setup.py
@@ -14,12 +14,9 @@ setup(
         'static/images/*.*'
     ]},
     include_package_data=True,
-    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
     entry_points = {
         'sphinx.html_themes': [
-            'name_of_theme = ioam_theme',
+            'ioam_theme = ioam_theme',
         ]
     },
 )
-
-
